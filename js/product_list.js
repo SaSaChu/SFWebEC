8c501c13c0f339d434e10b458ad448a8432ee6f2
CHO <bsuysheep927@gmail.com>
2024-03-07 00:25:46 +0800

    20240306 畫面調整、Bug 修復

--- js/product_list.js
+++ js/product_list.js
@@ -2,100 +2,115 @@ $(function() {
 	// sideMenu 
 	$('.closeSideBtn').click(function() {
 		$(this).hide();
-		$('.sideLeftMenu').hide();
+		
+		$('.sideLeftMenu').addClass('hide');
 		$('.openSideBtn').show();
 		$(this).parent().addClass('openBtn');
-		// $('#product-lists').addClass('flex-grow-1');
 		$('#product-lists').addClass('col-lg-10');
 	});
 
 	$('.openSideBtn').click(function() {
 		$(this).hide();
-		$('.sideLeftMenu').show();
+		$('.sideLeftMenu').removeClass('hide');
 		$('.closeSideBtn').show();
 		$(this).parent().removeClass('openBtn');
-		// $('#product-lists').removeClass('flex-grow-1');
 		$('#product-lists').removeClass('col-lg-10');
 	});
 	
 	// Ad
-	var swiperIg = new Swiper(".ad-swiper", {
+	var swiperAd = new Swiper(".ad-swiper", {
         slidesPerView: 1,
         watchSlidesProgress: true,
         loop: true,
+		autoplay: {
+            delay: 3000,
+            disableOnInteraction: true,
+        },
 		pagination: {
             el: ".swiper-pagination",
             clickable: true,
         },
-        // breakpoints: {
-            
-        //     768: {
-        //       slidesPerView: 4,
-        //       spaceBetween: 20,
-        //       watchSlidesProgress: true,
-        //       centeredSlides: false,
-        //     },
-        //     1025: {
-        //         slidesPerView: 7,
-        //         spaceBetween: 20,
-        //         centeredSlides: false,
-        //         watchSlidesProgress: true,
-        //         navigation: {
-        //             nextEl: '.igWeb-button-next',
-        //             prevEl: '.igWeb-button-prev',
-        //         },
-        //     }
-        // },
-        
     });
 
+	$('.color-item').on('mouseenter', function() {
+        $(this).addClass('active').siblings().removeClass('active');
+        $(this).parents('.card-body').prev().find("img").attr("src", $(this).attr('data-img'));
+        $(this).parent().next().find('p').text($(this).find('span').attr('data-colorName'));
+    });
 
 	$('.item-action-cart').on('click', function() {
-		$('.sub-card').remove();
-		let cartItem =`
-		<div class="card sub-card">
-			<div class="card-body">
-				<div class="color-wrapper">
-					<div class="color-name">
-						<p class="text-start">顏色：</p>
-						<span>灰色</span>
-					</div>
-					<div class="color-items d-flex">
-						<p class="color-item active"><span style="background-color: #818a9f;" data-color="灰色"></span></p>
-						<p class="color-item"><span style="background-color: #253766;" data-color="灰藍色"></span></p>
-						<p class="color-item"><span style="background-color: #c41232;" data-color="灰紅色"></span></p>
-					</div>
-				</div>	
-				<div class="size-wrapper">
-					<div class="size">
-						<p class="text-start">尺寸</p>
-					</div>	
-					<ul class="size-items">
-						<li class="size-item" data-size="xs">xs</li>
-						<li class="size-item" data-size="s">s</li>
-						<li class="size-item" data-size="m">m</li>
-						<li class="size-item active" data-size="l">l</li>
-					</ul>
-				</div>
-			</div>
-			<div class="action-wrapper">
-				<div class="row g-0">
-					<div class="col px-0">
-						<button class="btn btn-lg add-checkout">立即結帳</button>
-					</div>
-					<div class="col px-0">
-						<button class="btn btn-lg add-cart">加入購物車</button>
-					</div>
-				</div>
-			</div>
-		</div>`
-		if (!$(this).parents('.card-body').find('.sub-card').length) {
-			$(this).after(cartItem);
-		} else {
-			$('.sub-card').remove();
-		}
+        if (window.screen.width < 500) {
+            console.log("mobile");
+        } else {
+            let target = $(this).parent().find('.sub-card');
+            $('.sub-card').not(target).remove();
+            
+            let cartItem =`
+            <div class="sub-card">
+                <div class="card border-0">
+                    <div class="card-body">
+                        <div class="color-wrapper">
+                            <div class="color-name">
+                                <p class="text-start">顏色：</p>
+                                <span>灰色</span>
+                            </div>
+                            <div class="color-items d-flex">
+                                <p class="color-item active"><span style="background-color: #818a9f;" data-color="灰色"></span></p>
+                                <p class="color-item"><span style="background-color: #253766;" data-color="灰藍色"></span></p>
+                                <p class="color-item"><span style="background-color: #c41232;" data-color="灰紅色"></span></p>
+                            </div>
+                        </div>	
+                        <div class="size-wrapper">
+                            <div class="size">
+                                <p class="text-start">尺寸</p>
+                            </div>	
+                            <ul class="size-items">
+                                <li class="size-item" data-size="xs">xs</li>
+                                <li class="size-item" data-size="s">s</li>
+                                <li class="size-item" data-size="m">m</li>
+                                <li class="size-item active" data-size="l">l</li>
+                            </ul>
+                        </div>
+                        <div class="action-wrapper">
+                            <div class="row g-0">
+                                <div class="col px-0">
+                                    <button class="btn btn-lg add-checkout">立即結帳</button>
+                                </div>
+                                <div class="col px-0">
+                                    <button class="btn btn-lg add-cart">加入購物車</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>`;
 
+            if(!target.length) {
+                $(this).after(cartItem);
+                $('.sub-card .color-item').on('click', function() {
+                    $(this).addClass('active').siblings().removeClass('active');
+                    $(this).parent().prev().find('span').text($(this).find('span').attr('data-color')); 
+                    // $(this).addClass('active').siblings().removeClass('active');
+                    $(this).parents('.card-body').prev().find("img").attr("src", $(this).attr('data-img'));
+                    $(this).parents('.card-body').next().find('p').text($(this).find('span').attr('data-colorName'));
+                });
+    
+                $('.sub-card .size-item').on('click', function() {
+                    $(this).addClass('active').siblings().removeClass('active');
+                });
 
+                $('.sub-card').on('mouseleave', function() {
+                    console.log('out');
+                    $(this).remove();
+                })
+    
+                // TODO Ajax
+            } else {
+                target.remove();
+            }
+        }
+       
+       
 	});
 
 	$('.sort-item').on('shown.bs.tab', function(event) {
