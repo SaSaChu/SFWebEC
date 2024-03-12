883a29bab9c5c040eddfad9da79dda2a3c848663
CHO <bsuysheep927@gmail.com>
2024-03-12 16:28:26 +0800

    三花前端注意事項:3,5,8,9,10,11,16,17(缺購物車模組化)

--- js/home.js
+++ js/home.js
@@ -21,46 +21,50 @@ $(function () {
         
     });
 
-    // Category 
-    function addCategoryArrow() {
+    // Category & Top Mobile Swiper Arrow
+    function addMobileArrow(tabName) {
         
-        let itemLength = $('.category-item').length;
-        let activeItem = $('.category-item').find('.active');
-        let activeIndex = activeItem.parent().index() + 1;
+        let itemLength = $('.nav-'+tabName+'-item').length;
+        let activeItem = $('.nav-'+tabName+'-item').find('.active');
+        let activeIndex = activeItem.parent().index() + 1; 
         let activePrev =  (activeIndex - 1) == 0 ? itemLength : activeIndex - 1;
         let activeNext =  (activeIndex + 1) > itemLength ? 1 : activeIndex + 1; 
-        activeItem.before("<i class='fa-solid fa-arrow-left category-prev' data-action='prev' data-prev='"+activePrev+"'></i>")
-        activeItem.after("<i class='fa-solid fa-arrow-right category-next' data-action='next' data-next='"+activeNext+"'></i>")
 
-        $(".category-prev, .category-next").click(function(){
+        activeItem.before("<img src='images/btn/btn-up.svg' class='nav-tab-prev' data-action='prev' data-prev='"+activePrev+"'>")
+        activeItem.after("<img src='images/btn/btn-up.svg' class='nav-tab-next' data-action='next' data-next='"+activeNext+"'>") 
+
+        $(".nav-tab-prev, .nav-tab-next").click(function(){
             let action = $(this).attr('data-action');
-            $('.nav-link').removeClass('active');
-            $('.category-prev, .category-next').remove();
-            let categoryAction = $(this).attr('data-'+action);
-            $("#nav-category-tab-"+categoryAction).css("dsiplay:flex;");
-            $("#nav-category-tab-"+categoryAction).click();
-            addCategoryArrow(); 
+            $(this).parents('.nav-tabs').find('.nav-link').removeClass('active');
+            $(this).parents('.nav-tabs').find('.nav-tab-prev, .nav-tab-next').remove();
+            let tab = $(this).attr('data-'+action);
+            $("#nav-"+tabName+"-tab-"+tab).click().css("dsiplay:flex;");
+            addMobileArrow(tabName); 
         })
+            
     }
 
-    function removeCategoryArrow () {
-        $('.category-prev').remove();
-        $('.category-next').remove();
-    }
+    function removeMobileArrow () {
+        $('.nav-tab-prev').remove();
+        $('.nav-tab-next').remove();
+    } 
 
     if($(window).width() <= 768) {
-        addCategoryArrow();
+        addMobileArrow('category');
+        addMobileArrow('top');
+        
     } else {
-        removeCategoryArrow(); 
+        removeMobileArrow();
     }
 
     $(window).resize(function(){
         if($(window).width() <= 768) {
-            addCategoryArrow();
+            addMobileArrow('category');
+            addMobileArrow('top');
         } else {
-            removeCategoryArrow()
+            removeMobileArrow();
         }
-     })     
+    })     
     
 
     // Introduction
@@ -79,11 +83,11 @@ $(function () {
     var swiperNew = new Swiper(".new-swiper", {
         slidesPerView: 1,
         slidesPerGroup: 1,
-        spaceBetween: 30,
+        spaceBetween: 0,
         loop: true,
         navigation: {
-            nextEl: '.newMobile-button-next',
-            prevEl: '.newMobile-button-prev',
+            nextEl: '.new-button-next',
+            prevEl: '.new-button-prev',
         },
         
 
@@ -97,19 +101,11 @@ $(function () {
             1024: {
                 slidesPerView: 3,
                 slidesPerGroup: 3,
-                navigation: {
-                    nextEl: '.newWeb-button-next',
-                    prevEl: '.newWeb-button-prev',
-                },
             },
 
             1200: {
                 slidesPerView: 4,
                 slidesPerGroup: 4,
-                navigation: {
-                    nextEl: '.newWeb-button-next',
-                    prevEl: '.newWeb-button-prev',
-                },
             },
         },
     });
@@ -122,24 +118,46 @@ $(function () {
             el: ".swiper-pagination",
             clickable: true,
         },
+       
         breakpoints: {
-
             769: {
               slidesPerView: 3,
               spaceBetween: 20,
             },
         },
         
+        
     });
 
     // Store Info 
     var swiperStoreInfo = new Swiper(".storeInfo-swiper", {
+        slidesPerView: 1,
         spaceBetween: 10,
         freeMode: true,
         watchSlidesProgress: true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.storeMobile-button-next',
+            prevEl: '.storeMobile-button-prev',
+        },
+
+        breakpoints: {
+            769: {
+              navigation: {
+                nextEl: '.storeWeb-button-next',
+                prevEl: '.storeWeb-button-prev',
+                },
+            },
+        },
+        
         thumbs: {
             swiper: swiperStoreImg,
         },
+        
+        
     });
       
 
@@ -179,20 +197,41 @@ $(function () {
 
     $('.color-item').on('mouseenter', function() {
         $(this).addClass('active').siblings().removeClass('active');
-        $(this).parents('.card-body').prev().find("img").attr("src", $(this).attr('data-img'));
+        $(this).parents('.card').find(".card-img img").first().attr("src", $(this).attr('data-img'));
+        $(this).parents('.card').find(".card-img img").last().attr("src", $(this).attr('data-img-hover'));
         $(this).parent().next().find('p').text($(this).find('span').attr('data-colorName'));
         
     });
 
     $('.item-action-cart').on('click', function() {
+        
+
         if (window.screen.width < 500) {
-            console.log("mobile");
+            
+            let Offcanvas = $('#offcanvasBottom');
+            Offcanvas.on('show.bs.offcanvas', function() {
+                console.log(1);
+            })
+
+            Offcanvas.on('hidden.bs.offcanvas', function() {
+                Offcanvas.off('show.bs.offcanvs')
+                console.log('off')
+            })
+
+            // let Offcanvas = document.getElementById('offcanvasBottom');
+            // let test = Offcanvas.addEventListener('show.bs.offcanvas', function() {
+            //     console.log(1)
+            // });
+
+            // Offcanvas.addEventListener('hidden.bs.offcanvas', function() {
+            //     Offcanvas.
+            // })
         } else {
-            let target = $(this).parent().find('.sub-card');
-            $('.sub-card').not(target).remove();
+            let target = $(this).parent().find('.product-sub-card');
+            $('.product-sub-card').not(target).remove();
             
             let cartItem =`
-            <div class="sub-card">
+            <div class="product-sub-card">
                 <div class="card border-0">
                     <div class="card-body">
                         <div class="color-wrapper">
@@ -211,10 +250,10 @@ $(function () {
                                 <p class="text-start">尺寸</p>
                             </div>	
                             <ul class="size-items">
-                                <li class="size-item" data-size="xs">xs</li>
+                                <li class="size-item active" data-size="xs">xs</li>
                                 <li class="size-item" data-size="s">s</li>
                                 <li class="size-item" data-size="m">m</li>
-                                <li class="size-item active" data-size="l">l</li>
+                                <li class="size-item sell_out" data-size="l">l</li>
                             </ul>
                         </div>
                         <div class="action-wrapper">
@@ -233,20 +272,18 @@ $(function () {
 
             if(!target.length) {
                 $(this).after(cartItem);
-                $('.sub-card .color-item').on('click', function() {
+                $('.product-sub-card .color-item').on('click', function() {
                     $(this).addClass('active').siblings().removeClass('active');
                     $(this).parent().prev().find('span').text($(this).find('span').attr('data-color')); 
-                    // $(this).addClass('active').siblings().removeClass('active');
                     $(this).parents('.card-body').prev().find("img").attr("src", $(this).attr('data-img'));
                     $(this).parents('.card-body').next().find('p').text($(this).find('span').attr('data-colorName'));
                 });
     
-                $('.sub-card .size-item').on('click', function() {
+                $('.product-sub-card .size-item').on('click', function() {
                     $(this).addClass('active').siblings().removeClass('active');
                 });
 
-                $('.sub-card').on('mouseleave', function() {
-                    console.log('out');
+                $('.product-sub-card').on('mouseleave', function() {
                     $(this).remove();
                 })
     
@@ -255,8 +292,6 @@ $(function () {
                 target.remove();
             }
         }
-       
-       
 	});
-     
+
 });
