9042ee82d31f34f2c83572ca0bc5b3097e3ae404
CHO <bsuysheep927@gmail.com>
2024-03-20 00:25:47 +0800

    調整購物車 step2 畫面 & Js 功能

--- js/shopping_cart_1.js
+++ js/shopping_cart_1.js
@@ -1,47 +1,52 @@
 $(function() {
-	var swiperPoint = new Swiper(".point-swiper", {
-		slidesPerView: 1,
-		slidesPerGroup: 1,
-		// spaceBetween: 18,
-		loop: true,
-		navigation: {
-			nextEl: '.pointWeb-button-next',
-			prevEl: '.pointWeb-button-prev',
-		},
-		breakpoints: {
-			768: {
-				slidesPerView: 2,
-				slidesPerGroup: 2,
-			},
 
-			1024: {
-				slidesPerView: 4,
-				slidesPerGroup: 4,
+	// step 1
+	if($('.point-swiper').length) {
+		var swiperPoint = new Swiper(".point-swiper", {
+			slidesPerView: 1,
+			slidesPerGroup: 1,
+			// spaceBetween: 18,
+			loop: true,
+			navigation: {
+				nextEl: '.pointWeb-button-next',
+				prevEl: '.pointWeb-button-prev',
 			},
-		},
-	});
-
-	var swiperLike = new Swiper(".like-swiper", {
-		slidesPerView: 1,
-		slidesPerGroup: 1,
-		// spaceBetween: 30,
-		loop: true,
-		navigation: {
-			nextEl: '.likeWeb-button-next',
-			prevEl: '.likeWeb-button-prev',
-		},
-		breakpoints: {
-			768: {
-				slidesPerView: 2,
-				slidesPerGroup: 2,
+			breakpoints: {
+				768: {
+					slidesPerView: 2,
+					slidesPerGroup: 2,
+				},
+	
+				1024: {
+					slidesPerView: 4,
+					slidesPerGroup: 4,
+				},
 			},
-
-			1024: {
-				slidesPerView: 4,
-				slidesPerGroup: 4,
+		});
+	}
+	
+	if($('.like-swiper').length) {
+		var swiperLike = new Swiper(".like-swiper", {
+			slidesPerView: 1,
+			slidesPerGroup: 1,
+			loop: true,
+			navigation: {
+				nextEl: '.likeWeb-button-next',
+				prevEl: '.likeWeb-button-prev',
 			},
-		},
-	});
+			breakpoints: {
+				768: {
+					slidesPerView: 2,
+					slidesPerGroup: 2,
+				},
+	
+				1024: {
+					slidesPerView: 4,
+					slidesPerGroup: 4,
+				},
+			},
+		});
+	}
 
 	$('.count_btn').on('click', function (){
 		let countInput = $(this).parent().find('.count_value') 
@@ -75,9 +80,7 @@ $(function() {
 				$(this).addClass('active').siblings().removeClass('active');	
 			}
 		})
-	})
-
-	
+	})	
 
 	// 點擊也許你也喜歡顏色
 	$('.like-item .color-item').click(function() {
@@ -85,18 +88,7 @@ $(function() {
 		$(this).parents('.product-card').find(".card-img img").first().attr("src", $(this).attr('data-img'));
         $(this).parents('.product-card').find(".card-img img").last().attr("src", $(this).attr('data-img-hover'));
         $(this).parent().next().find('p').text($(this).attr('data-color-name'));  
-	})
-
-	// $('.check_label').click(function(){
-	// 	if ($('#check-all-cart').is(":checked")) 
-	// 	{
-	// 		console.log(1)
-	// 	} else {
-	// 		console.log(2);
-	// 	}
-
-	// 	console.log($('#check-all-cart').val());
-	// })
+	})	
 
 	$('#check-all-cart').on('change', function() {
 		if ($(this).is(":checked")) 
@@ -115,5 +107,61 @@ $(function() {
 			console.log(2);
 		}	
 	})
+
+	function deliveryType(type) {
+		console.log(type);
+		let allType = $('.delivery-type');
+		let activeType = $('.delivery-'+type);
+		allType.hide();
+		allType.find('input, select').attr('disabled', 'disabled');
+		activeType.find('input, select').removeAttr('disabled');
+		activeType.show();
+	}
+
+	function invoiceType(type) {
+		console.log(type);	
+		let allType = $('.invoice-type');
+		let activeType = $('.invoice-'+type);
+		allType.hide();
+		allType.find('input, select').attr('disabled', 'disabled');
+		activeType.find('input, select').removeAttr('disabled');
+		activeType.show();
+	}
+
+	//step 2 
+	let delivery = $('input[name="delivery_type"]'); 
+	if(delivery.length) {
+		delivery.on('change', function() {
+			deliveryType($(this).val())
+			
+		})
+	}
+
+	// 選擇常用門市
+	let storeModal = $('#storeModal');
+	storeModal.on('show.bs.modal', function() {
+		// Ajax 抓資料
+		console.log('storeModal Ajax')
+	});
+	
+	// 發票資料
+	let invoice =  $('select[name="invoice_type"]'); 
+	invoice.on('change', function() {
+		invoiceType($(this).val());
+	})
+
+	$('.sync-name').on('click', function() {
+		$(this).addClass('active').parents('.col').siblings().find('.radio_label').removeClass('active');
+		
+		$('input[name="invoice_name"]').val($('input[name="'+$(this).attr("data-target")+'"]').val())
+	})
+	
+	// 提交完成訂單
+	let chekcOrder = $('.check-order') 
+	if(chekcOrder.length) {
+		chekcOrder.on('click', function() {
+			console.log($('#step-second-form').serializeArray());
+		})
+	}
 	
 })
