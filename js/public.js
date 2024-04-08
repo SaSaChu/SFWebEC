3b784df91f47c172b2c9b747f9e69e76f63a4189
SaSaChu <teresa7397@gmail.com>
2024-04-08 18:40:56 +0800

    會員紅利點數清單-over

--- js/public.js
+++ js/public.js
@@ -138,5 +138,10 @@ $(function () {
             $('.goTop').removeClass('show'); 
         } 
     })
+
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+    var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl)
+})
      
 });
